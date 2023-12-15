cda9f7a95347d11281185eb1a1e03aed981131db
shannandreii <143739759+shannandreii@users.noreply.github.com>
2023-12-15 10:05:12 +0800

    move

---
+++ src/main/java/com/appdev/codetech/Entity/UserEntity.java
@@ -0,0 +1,111 @@
+package com.appdev.codetech.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbluser")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userid;
+    @Column(name = "username", unique = true)
+    private String username;
+    @Column(name = "email", unique = true)
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "firstname")
+    private String firstname;
+    @Column(name = "lastname")
+    private String lastname;
+    @Column(name = "role")
+    private String role;
+    @Column(name = "isDelete")
+    private boolean isDelete;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(int userid, String username, String email, String password, String firstname, String lastname,
+            String role,
+            boolean isDelete) {
+        this.userid = userid;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.role = role;
+        this.isDelete = isDelete;
+    }
+
+    public int getUserid() {
+        return this.userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return this.firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return this.lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean getIsDelete() {
+        return this.isDelete;
+    }
+
+    public void setIsDelete(boolean isDelete) {
+        this.isDelete = isDelete;
+    }
+
+}
---
+++ src/main/java/com/appdev/codetech/Service/UserService.java
@@ -0,0 +1,136 @@
+package com.appdev.codetech.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.appdev.codetech.Entity.UserEntity;
+import com.appdev.codetech.Repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository urepo;
+
+    public UserEntity insertUser(UserEntity user) {
+        return urepo.save(user);
+    }
+
+    public Map<String, Object> checkEmailExists(String email) {
+        Map<String, Object> result = new HashMap<>();
+        try {
+            Optional<UserEntity> existingUser = urepo.findByEmail(email);
+            result.put("exists", existingUser.isPresent());
+            result.put("userid", existingUser.map(UserEntity::getUserid).orElse(null));
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.put("exists", false);
+            result.put("userid", null);
+        }
+        return result;
+    }
+
+    public Map<String, Object> checkUsernameExists(String username) {
+        Map<String, Object> result = new HashMap<>();
+        try {
+            Optional<UserEntity> existingUser = urepo.findByUsername(username);
+            result.put("exists", existingUser.isPresent());
+            result.put("userid", existingUser.map(UserEntity::getUserid).orElse(null));
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.put("exists", false);
+            result.put("userid", null);
+        }
+        return result;
+    }
+
+    public List<UserEntity> getAllUsers() {
+        return urepo.findAll();
+    }
+
+    public UserEntity updateUser(int userid, UserEntity newUserEntityDetails) {
+        UserEntity user = new UserEntity();
+        try {
+            user = urepo.findById(userid).get();
+            user.setUsername(newUserEntityDetails.getUsername());
+            user.setPassword(newUserEntityDetails.getPassword());
+            user.setEmail(newUserEntityDetails.getEmail());
+            user.setFirstname(newUserEntityDetails.getFirstname());
+            user.setLastname(newUserEntityDetails.getLastname());
+            user.setRole(newUserEntityDetails.getRole());
+            user.setIsDelete(newUserEntityDetails.getIsDelete());
+        } catch (NoSuchElementException ex) {
+            throw new NoSuchElementException("User: " + userid + " does not exist!");
+        }
+
+        return urepo.save(user);
+
+    }
+
+    public UserEntity removeUser(int userid) {
+        UserEntity user = new UserEntity();
+        try {
+            user = urepo.findById(userid).get();
+            user.setUsername(null);
+            user.setPassword(null);
+            user.setEmail(null);
+            user.setFirstname(null);
+            user.setLastname(null);
+            user.setRole(null);
+            user.setIsDelete(true);
+        } catch (NoSuchElementException ex) {
+            throw new NoSuchElementException("User: " + userid + " does not exist!");
+        }
+
+        return urepo.save(user);
+
+    }
+
+    public AuthenticationResult authenticateUser(String usernameOrEmail, String password) {
+        try {
+            Optional<UserEntity> user;
+
+            if (usernameOrEmail.contains("@")) {
+                user = urepo.findByEmail(usernameOrEmail);
+            } else {
+                user = urepo.findByUsername(usernameOrEmail);
+            }
+
+            if (user.isPresent() && user.get().getPassword().equals(password)) {
+                AuthenticationResult result = new AuthenticationResult(true, false, false, user.get());
+                return result;
+            } else {
+                boolean invalidPassword = false;
+                boolean usernameOrEmailNotFound = false;
+
+                if (user.isPresent()) {
+                    invalidPassword = true;
+                } else {
+                    usernameOrEmailNotFound = true;
+                }
+
+                return new AuthenticationResult(false, invalidPassword, usernameOrEmailNotFound, null);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new AuthenticationResult(false, true, false, null);
+        }
+    }
+
+    public String deleteUser(int userid) {
+        String msg = "";
+
+        if (urepo.findById(userid) != null) {
+            urepo.deleteById(userid);
+            msg = "User " + userid + " is successfully deleted!";
+        } else
+            msg = "User " + userid + " does not exist";
+
+        return msg;
+    }
+}
